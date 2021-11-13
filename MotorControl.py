4816037d71b4ab23ef0becec0b6697ddf8f51c1c
Dorothy <dorothy.harris@maine.edu>
2021-11-12 19:59:42 -0500

    Added edited motor control GPIO values

--- MotorControl.py
+++ MotorControl.py
@@ -176,13 +176,13 @@ GPIO.setup(19, GPIO.IN)     # 8
 
 
 # GPIO pins setup with RPi (WILL NEED TO BE CHANGED!)
-GPIO.setup(7, GPIO.OUT)     # PWMA
-GPIO.setup(11, GPIO.OUT)    # AIN2
-GPIO.setup(12, GPIO.OUT)    # AIN1
-GPIO.setup(13, GPIO.OUT)    # STBY
-GPIO.setup(15, GPIO.OUT)    # BIN1
-GPIO.setup(16, GPIO.OUT)    # BIN2
-GPIO.setup(18, GPIO.OUT)    # PWMB
+GPIO.setup(25, GPIO.OUT)    # PWMA
+GPIO.setup(8, GPIO.OUT)     # AIN2
+GPIO.setup(7, GPIO.OUT)     # AIN1
+GPIO.setup(12, GPIO.OUT)    # STBY
+GPIO.setup(16, GPIO.OUT)    # BIN1
+GPIO.setup(20, GPIO.OUT)    # BIN2
+GPIO.setup(21, GPIO.OUT)    # PWMB
 
 # Create the socket object
 s = socket.socket()
@@ -306,70 +306,70 @@ while True:
         if motor1 < Current1:
             
             # Clockwise control of Motor A
-            GPIO.output(12, GPIO.HIGH)  # Set AIN1
-            GPIO.output(11, GPIO.LOW)   # Set AIN2
+            GPIO.output(7, GPIO.HIGH)  # Set AIN1
+            GPIO.output(8, GPIO.LOW)   # Set AIN2
             
             # Set motor speed with PWMA
-            GPIO.output(7, GPIO.HIGH)   # Motor A
+            GPIO.output(25, GPIO.HIGH)   # Motor A
             
             #Disable standby with STBY
-            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(12, GPIO.HIGH)
 
         elif motor1 > Current1:
 
             # Counterclockwise control of Motor A
-            GPIO.output(12, GPIO.LOW)  # Set AIN1
-            GPIO.output(11, GPIO.HIGH)   # Set AIN2
+            GPIO.output(7, GPIO.LOW)  # Set AIN1
+            GPIO.output(8, GPIO.HIGH)   # Set AIN2
             
             # Set motor speed with PWMA
-            GPIO.output(7, GPIO.HIGH)   # Motor A
+            GPIO.output(25, GPIO.HIGH)   # Motor A
             
             #Disable standby with STBY
-            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(12, GPIO.HIGH)
 
         # Wait until at correct position
         while motor1 != Current1:
 
         # Turn off Motor A
-        GPIO.output(7, GPIO.LOW)     # PWMA
-        GPIO.output(11, GPIO.LOW)    # AIN2
-        GPIO.output(12, GPIO.LOW)    # AIN1
-        GPIO.output(13, GPIO.LOW)    # STBY
+        GPIO.output(25, GPIO.LOW)     # PWMA
+        GPIO.output(8, GPIO.LOW)    # AIN2
+        GPIO.output(7, GPIO.LOW)    # AIN1
+        GPIO.output(12, GPIO.LOW)    # STBY
 
 
         if motor2 < Current2:
 
             # Clockwise control of Motor B
-            GPIO.output(15, GPIO.HIGH)  # Set BIN1
-            GPIO.output(16, GPIO.LOW)   # Set BIN2
+            GPIO.output(16, GPIO.HIGH)  # Set BIN1
+            GPIO.output(20, GPIO.LOW)   # Set BIN2
             
             # Set motor speed with PWMB
-            GPIO.output(18, GPIO.HIGH)  # Motor B
+            GPIO.output(21, GPIO.HIGH)  # Motor B
             
             #Disable standby with STBY
-            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(12, GPIO.HIGH)
 
 
         elif motor2 > Current2:
 
             # Counterclockwise control of Motor B
-            GPIO.output(15, GPIO.LOW)  # Set BIN1
-            GPIO.output(16, GPIO.HIGH)   # Set BIN2
+            GPIO.output(16, GPIO.LOW)  # Set BIN1
+            GPIO.output(20, GPIO.HIGH)   # Set BIN2
             
             # Set motor speed with PWMB
-            GPIO.output(18, GPIO.HIGH)  # Motor B
+            GPIO.output(21, GPIO.HIGH)  # Motor B
             
             #Disable standby with STBY
-            GPIO.output(13, GPIO.HIGH)
+            GPIO.output(12, GPIO.HIGH)
 
         # Wait until at correct position
         while motor2 != Current2:
 
         # Turn off Motor B
-        GPIO.output(13, GPIO.LOW)    # STBY
-        GPIO.output(15, GPIO.LOW)    # BIN1
-        GPIO.output(16, GPIO.LOW)    # BIN2
-        GPIO.output(18, GPIO.LOW)    # PWMB
+        GPIO.output(12, GPIO.LOW)    # STBY
+        GPIO.output(16, GPIO.LOW)    # BIN1
+        GPIO.output(20, GPIO.LOW)    # BIN2
+        GPIO.output(21, GPIO.LOW)    # PWMB
 
 
     except socket.error:
@@ -383,13 +383,13 @@ c.close()
 # Reset all GPIO pins
 # Make sure everything has stopped moving
 # Not sure if needed
-GPIO.output(7, GPIO.LOW)     # PWMA
-GPIO.output(11, GPIO.LOW)    # AIN2
-GPIO.output(12, GPIO.LOW)    # AIN1
-GPIO.output(13, GPIO.LOW)    # STBY
-GPIO.output(15, GPIO.LOW)    # BIN1
-GPIO.output(16, GPIO.LOW)    # BIN2
-GPIO.output(18, GPIO.LOW)    # PWMB
+GPIO.output(25, GPIO.LOW)     # PWMA
+GPIO.output(8, GPIO.LOW)    # AIN2
+GPIO.output(7, GPIO.LOW)    # AIN1
+GPIO.output(12, GPIO.LOW)    # STBY
+GPIO.output(16, GPIO.LOW)    # BIN1
+GPIO.output(20, GPIO.LOW)    # BIN2
+GPIO.output(21, GPIO.LOW)    # PWMB
 
 # Release GPIO pins
 GPIO.cleanup()
